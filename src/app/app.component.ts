6d81eba0a0bbbaea031d286b8891463985bdce8d
Danny Blue <dannybluedesign@gmail.com>
2017-02-27 14:48:37 -0500

    sorry forgot to push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { JQUERY_TOKEN } from './jquery-provider';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(@Inject(JQUERY_TOKEN) $) {
+    console.log($);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, OpaqueToken } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { JQUERY_PROVIDER } from './jquery-provider';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [JQUERY_PROVIDER],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
